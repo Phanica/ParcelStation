67f102ac9a1c3fc024671f25d0be4b6d13c960dc
masterhonia <121284184+Phanica@users.noreply.github.com>
2025-04-19 21:07:38 +0800

    refactor(client): 合并main.h和main.c中的定义到common.h和common.c
    将main.h和main.c中的定义和逻辑合并到common.h和common.c中，以提高代码的维护性和减少重复代码。删除不再使用的main.h和main.c文件。

--- client/common.h
+++ client/common.h
@@ -1,10 +1,95 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-#include "main.h"  // 包含原有定义
+// #include "main.h"  // 包含原有定义
 
-size_t _placeholder_1_in_common_h;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <winsock2.h>
 
+#define MAX_USERNAME_LENGTH 32
+#define MAX_PASSWORD_LENGTH 32
+#define MAX_TASK_ID_LENGTH 32
+#define MAX_ARGS_LENGTH 256
+#define MAX_HEADER_LENGTH 256
+#define MAX_REQ_LENGTH 256
+#define MAX_MESSAGE_SIZE 2048
+#define MAX_RESPONSE_SIZE 2048
+
+typedef enum UserType {
+    postman,
+    manager,
+    user,
+} UserType;
+
+typedef enum RequestType {
+    signup,
+    login,
+    logout,
+    get,
+    list,
+    assign,
+    report_loss,
+    statistics,
+    query,
+    delete
+} RequestType;
+
+typedef struct User {
+    UserType type;
+    char username[MAX_USERNAME_LENGTH];
+    char password[MAX_PASSWORD_LENGTH];
+} User;
+
+typedef struct SignupLoginArguments {
+    char username[MAX_USERNAME_LENGTH];
+    char password[MAX_PASSWORD_LENGTH];
+} SignupLoginArguments;
+
+typedef struct AssignReportArguments {
+    char id[MAX_TASK_ID_LENGTH];
+} AssignReportArguments;
+
+typedef union RequestArguments {
+    SignupLoginArguments signup_login;
+    AssignReportArguments assign_report;
+} RequestArguments;
+
+typedef struct Request {
+    RequestType type;
+    RequestArguments arguments;
+} Request;
+
+typedef struct Message {
+    User user;
+    Request request;
+} Message;
+
+typedef struct Response {
+    char status[32];
+    char message[256];
+} Response;
+
+typedef struct ClientData {
+    char username[MAX_USERNAME_LENGTH];
+    char password[MAX_PASSWORD_LENGTH];
+    int has_login;
+    UserType type;
+} ClientData;
+
+ClientData client_data;
+SOCKET sock;
+WSADATA wsaData;
+
+// 公共函数声明
+void init_connection();
+void close_connection();
+void send_request(Message *msg);
+void handle_login(const char *username, const char *password);
+int serialize_message(const Message *msg, char *buffer);
+void deserialize_response(const char *buffer, Response *res);
+size_t strsize(const char *str);
 void init_connection();
 void close_connection();
 void send_request(Message *msg);
